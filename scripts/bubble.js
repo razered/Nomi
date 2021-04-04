c8de1c1d588ced8360f747d0aa62170bf2ae5b37
curefit-aditya-2020 <aditya.bhagwat@curefit.com>
2021-04-05 01:51:08 +0530

    Finalise website. No further cosmetic changes allowed whatsover

---
+++ scripts/bubble.js
@@ -0,0 +1,92 @@
+(function() {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)    
+    var width = 800;
+    var height = 800;
+
+    var svg = d3.select(".chart")
+                .append("svg")
+                .attr("height", height)
+                .attr("width", width)
+                .append("g")
+                .attr("transform", "translate(0,0)")
+
+    var defs = svg.append("defs");
+
+    defs.append("pattern")
+        .attr("id", "jon-snow")
+        .attr("height", "100%")
+        .attr("width", "100%")
+        .attr("patternContentUnits", "objectBoundingBox")
+        .append("image")
+        .attr("width", 1)
+        .attr("height", 1)
+        .attr("preserveAspectRatio", "none")
+        .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+        .attr("xlink:href", "img/skills/tools/aws.jpg");
+
+    var radiusScale = d3.scaleSqrt().domain([1, 11]).range([10, 80])
+    // console.log(svg)
+
+    // the simulation is a collection of forces about where we want our circles to go and how we want our circles to interact
+    var simulation = d3.forceSimulation()
+                    .force("x", d3.forceX(width / 2).strength(0.05))
+                    .force("y", d3.forceY(height / 2).strength(0.05))
+                    .force("collide", d3.forceCollide(function(d) {
+                        return radiusScale(d.size) + 1
+                    }))
+
+    d3.queue()
+        .defer(d3.csv, "./skillset.csv")
+        .await(ready)
+
+    function ready (error, datapoints) {
+        defs.selectAll(".skill-pattern")
+            .data(datapoints)
+            .enter().append("pattern")
+            .attr("class", "skill-pattern")
+            .attr("id", function(d) {
+                return d.id
+            })
+            .attr("height", "100%")
+            .attr("width", "100%")
+            .attr("patternContentUnits", "objectBoundingBox")
+            .append("image")
+            .attr("width", 1)
+            .attr("height", 1)
+            .attr("preserveAspectRatio", "none")
+            .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+            .attr("xlink:href", function(d){
+                return d.img
+            });
+
+
+        var circles = svg.selectAll(".skills")
+            .data(datapoints)
+            .enter().append("circle")
+            .attr("class", "skills")
+            .attr("r", function(d){
+                console.log(radiusScale(d.size))
+                return radiusScale(d.size)
+            })
+            .attr("fill", function(d) {
+                return "url(#" + d.id + ")"
+            })
+            .on("click", function(d) {
+                console.log(d)
+            })
+
+        simulation.nodes(datapoints)
+            .on('tick', ticked)
+
+        function ticked() {
+            circles
+                .attr("cx", function(d) {
+                    return d.x
+                })
+                .attr("cy", function(d) {
+                    return d.y
+                })
+        }
+    }
+})();
--- skills.md
+++ skills.md
@@ -1,56 +1,21 @@
 ---
 layout: page
-title: Resume
-description: Things I think I know
+title: Skills
+description: Things I've built and associated source code.
+body-class: skills
 # redirect_from:
-#   - /skills/
+#   - /projects/
 ---
+
 # Skills
 
-The following is a list of technologies I know and like using.
+<div class="tout tout--secondary">
+<p> Here are the technologies I have experience in. Find my detailed resume <a href="https://drive.google.com/file/d/1FtsqfEGkxXgcLETegz7227zdJFWPPXEO/view?usp=sharing">here.</a></p> 
 
+<!-- or mini applications -->
+</div> 
 
-## Languages
--  Python 
-
-- Typescript
-
-- Haskell
-
-- Racket
-
-- Java
-
-- Bash
-
-
-
-
-## Frameworks & Platforms
-- Node/Express
-
-- Spring JPA
-
-- Django
-
-- Pytorch
-
-- AWS
-
-## Tools
-- k8s
-
-- Spinakker
-
-- Jenkins
-
-- Swagger/OpenAPI
-
-- Robo3T, MySQL Workbench
-
-## Data Stores
-- MySQL
-- neo4j
-- MongoDB
-- Redis
-
+<div class="chart"></div>
+<!-- <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.2/d3.min.js"></script>
+<script src="/scripts/bubble.js"></script>
